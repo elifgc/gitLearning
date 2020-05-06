e4f4645eb3c1bdae0b919f8adbc10b1607ceeaf8
elifgc <gencelif3406@gmail.com>
2020-05-06 18:22:56 +0300

    add Spring.java

---
+++ src/tests/Spring.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class Spring {
+
+    public static void main(String[] args) {
+
+        System.out.println("Please click logout button");
+        System.out.println("welcome");
+        System.out.println("Hello new world again");
+    }
+}
