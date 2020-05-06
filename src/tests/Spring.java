511b40b284b94f1e2fa28af06a8fae5adcd62d25
elifgc <gencelif3406@gmail.com>
2020-05-06 18:24:49 +0300

    update Spring.java

--- src/tests/Spring.java
+++ src/tests/Spring.java
@@ -4,8 +4,6 @@ public class Spring {
 
     public static void main(String[] args) {
 
-        System.out.println("Please click logout button");
-        System.out.println("welcome");
-        System.out.println("Hello new world again");
+        System.out.println("Good bye spring...");
     }
 }
